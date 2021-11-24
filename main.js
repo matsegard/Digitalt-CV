1f23472d0112c769bd87051f28bb49745a1d1bd6
matsegard <matsegard@outlook.com>
2021-11-24 15:33:07 +0100

    Added hamburger menu

---
+++ main.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', main);
+
+function main() {
+    addEventListeners();
+}
+
+function addEventListeners() {
+    const menuButton = document.getElementById('menu-button');
+    menuButton.addEventListener('click', toggleMenu);
+}
+
+function toggleMenu() {
+   const dropDown = document.getElementById('drop-menu');
+   dropDown.style.
+}
