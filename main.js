cb7a016053f6156e525ce975fb1c49a06b151226
matsegard <matsegard@outlook.com>
2021-11-25 16:02:45 +0100

    added scroll pages

--- main.js
+++ main.js
@@ -8,28 +8,58 @@ function addEventListeners() {
     const menuButton = document.getElementById('menu-button');
     menuButton.addEventListener('click', toggleMenu);
 
-    const contactButton = document.getElementById("about-me-page");
-    contactButton.addEventListener('click', showAboutMePage)
+    const homeClick = document.getElementById('home-page');
+    homeClick.addEventListener('click', showHomePage);
+    homeClick.addEventListener('click', hideNavbar);
+
+    const aboutMeClick = document.getElementById('about-me-page');
+    aboutMeClick.addEventListener('click', showAboutMePage);
+    aboutMeClick.addEventListener('click', hideNavbar);
+    
+    const contactClick = document.getElementById('contact-page');
+    contactClick.addEventListener('click', showContactPage);
+    contactClick.addEventListener('click', hideNavbar);
 }
 
 function toggleMenu() {
-   const dropDown = document.getElementById('drop-menu');
-   
-   if (dropDown.style.display === 'none') {
-    dropDown.style.display = 'block';
-} else {
-    dropDown.style.display = 'none';
-}
+    const dropDown = document.getElementById('drop-menu');
+
+    if (dropDown.style.display === 'none') {
+        dropDown.style.display = 'block';
+    } else {
+        dropDown.style.display = 'none';
+    }
 }
 
-function showAboutMePage(){
-   const elmnt = document.getElementById('about-me');
-   
-   elmnt.scrollIntoView({
+function hideNavbar() {
+    const dropmenu = document.getElementById('drop-menu');
+    dropmenu.style.display = 'none'
+}
+
+function showHomePage() {
+    const scrollToHome = document.getElementById('header');
+
+    scrollToHome.scrollIntoView({
         behavior: "smooth",
         block: "start",
-      });
+    });
+}
 
-    }
+function showAboutMePage() {
+    const scrollToAboutMe = document.getElementById('about-me');
 
+    scrollToAboutMe.scrollIntoView({
+        
+        behavior: "smooth",
+        block: "start",
+    });
+}
 
+function showContactPage() {
+    scrollToContactPage = document.getElementById('contact-me');
+
+    scrollToContactPage.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+    });
+}
