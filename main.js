47cf0e2c10c136784ce748f69c422aa66ebee8dd
matsegard <matsegard@outlook.com>
2021-11-25 14:03:08 +0100

    added scroll into view

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ function main() {
 function addEventListeners() {
     const menuButton = document.getElementById('menu-button');
     menuButton.addEventListener('click', toggleMenu);
+
+    const contactButton = document.getElementById("about-me-page");
+    contactButton.addEventListener('click', showAboutMePage)
 }
 
 function toggleMenu() {
@@ -18,4 +21,15 @@ function toggleMenu() {
     dropDown.style.display = 'none';
 }
 }
+
+function showAboutMePage(){
+   const elmnt = document.getElementById('about-me');
+   
+   elmnt.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+
+    }
+
 
