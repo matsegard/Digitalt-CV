52532dfba6d48073614c6a71214cf5eaf3bf1a3c
matsegard <matsegard@outlook.com>
2021-11-29 20:14:34 +0100

    added change icon onlick

--- main.js
+++ main.js
@@ -1,29 +1,36 @@
 window.addEventListener('load', main);
 
+
 function main() {
     addEventListeners();
 }
 
 function addEventListeners() {
     const menuButton = document.getElementById('menu-button');
+    const homeClick = document.getElementById('home-page');
+    const aboutMeClick = document.getElementById('about-me-page');
+    const contactClick = document.getElementById('contact-page');
+    const portfolioClick = document.getElementById('portfolio-page');
+    
+    menuButton.addEventListener('click', toggleIcon);
+    
     menuButton.addEventListener('click', toggleMenu);
 
-    const homeClick = document.getElementById('home-page');
-    homeClick.addEventListener('click',  scrollToHomeSection);
+    homeClick.addEventListener('click', scrollToHomeSection);
     homeClick.addEventListener('click', hideNavbar);
 
-    const aboutMeClick = document.getElementById('about-me-page');
+
     aboutMeClick.addEventListener('click', scrollToAboutMeSection);
     aboutMeClick.addEventListener('click', hideNavbar);
-    
-    const contactClick = document.getElementById('contact-page');
+
+
     contactClick.addEventListener('click', scrollToContactSection);
     contactClick.addEventListener('click', hideNavbar);
 
-    const portfolioClick = document.getElementById('portfolio-page');
+
     portfolioClick.addEventListener('click', scrollToPortfolioSection);
     portfolioClick.addEventListener('click', hideNavbar);
-   
+
 }
 
 function toggleMenu() {
@@ -44,7 +51,7 @@ function hideNavbar() {
 function scrollToHomeSection() {
     const homeSection = document.getElementById('header');
 
-   homeSection.scrollIntoView({
+    homeSection.scrollIntoView({
         behavior: "smooth",
         block: "start",
     });
@@ -54,7 +61,7 @@ function scrollToAboutMeSection() {
     const aboutMeSection = document.getElementById('about-me');
 
     aboutMeSection.scrollIntoView({
-        
+
         behavior: "smooth",
         block: "start",
     });
@@ -62,7 +69,7 @@ function scrollToAboutMeSection() {
 
 function scrollToContactSection() {
     const contactSection = document.getElementById('contact-me');
-    
+
     contactSection.scrollIntoView({
         behavior: "smooth",
         block: "start",
@@ -77,3 +84,15 @@ function scrollToPortfolioSection() {
         block: "start",
     });
 }
+
+function toggleIcon (){
+    const iconChange = document.getElementById("menu-button");
+    
+     if(iconChange.innerHTML === 'menu'){
+       iconChange.innerHTML = 'close';
+     } else {
+        iconChange.innerHTML = 'menu';
+     }
+}
+
+    
